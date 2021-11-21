24609afc0223dc8824878dd0155e75b4e1309978
david <david78787878787@gmail.com>
2021-11-21 12:59:53 +0100

    Konstruktory (continuation) P.S dynamic array of objects

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,17 +1,22 @@
 #include "funs.h"
-using namespace std;
 
-int main() {
-  samochod cars[] = {{"Citroen", "rapid", 2016, "gray", 5},
-                     {"Ford", "Mustang", 2012, "yellow", 7},
-                     {"Ferrari", "F40", 1975, "Red"},
-                     {"Ferrari", "F50", 1980, "yellow"},
-                     {"Ford", "Focus", 2010, "darkblue"},
-                     {"Ford", "Explorer", 2011, "white"},
-                     {"Alpha Omega", "Romeo", 2008, "whitish gray"}};
-    int N = sizeof(cars)/sizeof(cars[0]);
-    printAllObjects(cars,N);
-    printRepeating(cars,N);
-    sortObjectsByYear(cars,N);
+int main()
+{
+    const int len = 3;
+
+    figura *dyn = new figura[len]{
+         figura(666,999),
+         figura(),
+         figura(69,69)
+    };
+    dyn[333] = figura(123,321);
+    dyn[333].print();
+    dyn[0].print();
+    dyn[1].print();
+    dyn[2].print();
+    dyn[100] = figura(300,1200);
+    dyn[20] = figura(200);
+    dyn[20].print();
+    delete []dyn;
     return 0;
 }
